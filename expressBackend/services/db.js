f57d1a788741c9b08d9aa94529eec44c2305d913
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2021-04-06 02:17:03 +0300

    deploy to heroku

--- expressBackend/services/db.js
+++ expressBackend/services/db.js
@@ -1,10 +1,15 @@
-const { Pool } = require("pg");
+const { Pool, Client } = require("pg");
 const config = require("../config");
-const pool = new Pool(config.db);
+// const pool = new Pool(config.db);
 
 async function query(query, params) {
-  const { rows, fields } = await pool.query(query, params);
+  const client = new Client(config.db);
+  await client.connect();
 
+  // const { rows, fields } = await pool.query(query, params);
+  const { rows, fields } = await client.query(query, params);
+
+  await client.end();
   return rows;
 }
 
